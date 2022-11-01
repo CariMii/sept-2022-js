37fdfbc2647bc02c502d42e1277494981848c06b
CariMii <miskokarina98@gmail.com>
2022-11-01 16:38:41 +0100

    Js

---
+++ homework1/main.js
@@ -0,0 +1,32 @@
+// task1
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = '1';
+let f = '10';
+let g = '-999';
+let h = '123';
+let i = '3.14';
+let j = '2.7';
+let k = '16';
+let l = 'true';
+let m = 'false';
+console.log(a,b,c,d,e,f,g,h,i,j,k)
+
+// task2
+let firstName = 'Misko';
+let middleName = 'Karina';
+let lastName = 'Viktorivna';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+// task3
+let a1 = 100;
+console.log(a1);
+
+let b1 = '100';
+console.log(b1);
+
+let c1 = 'true';
+console.log(typeof c1);
